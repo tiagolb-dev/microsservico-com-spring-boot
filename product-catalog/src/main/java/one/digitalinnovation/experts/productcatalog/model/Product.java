06236c886875ea7672265d79c3841ccf8b056e48
tiagolb-dev <tiagospi@hotmail.com>
2021-08-23 23:14:47 -0300

    Added class Product

---
+++ product-catalog/src/main/java/one/digitalinnovation/experts/productcatalog/model/Product.java
@@ -0,0 +1,32 @@
+package one.digitalinnovation.experts.productcatalog.model;
+
+public class Product {
+
+    private Integer id;
+    private String name;
+    private Integer amount;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Integer amount) {
+        this.amount = amount;
+    }
+}
