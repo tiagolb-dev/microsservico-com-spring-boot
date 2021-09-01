fba0e2523d2f9b468be134951c788a8a8050481c
tiagolb-dev <tiagospi@hotmail.com>
2021-09-01 16:53:06 -0300

    Replace

--- product-catalog/src/main/java/one/digitalinnovation/experts/productcatalog/model/Product.java
+++ product-catalog/src/main/java/one/digitalinnovation/experts/productcatalog/model/Product.java
@@ -1,7 +1,12 @@
 package one.digitalinnovation.experts.productcatalog.model;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+
+@Document(indexName = "product", type= "catalog")
 public class Product {
 
+    @Id
     private Integer id;
     private String name;
     private Integer amount;
